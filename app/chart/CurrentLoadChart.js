f7239cda11df1c6eb7edbe4542fae2ea47ae5c2b
DJ-BoT07 <djmangaonkar7@gmail.com>
2024-11-27 15:00:48 +0530

    Done

--- app/chart/CurrentLoadChart.js
+++ app/chart/CurrentLoadChart.js
@@ -184,7 +184,7 @@ export default function CurrentLoadChart({ date }) {
               <YAxis stroke="#fff" />
               <Tooltip 
                 contentStyle={{ 
-                  backgroundColor: '#333', 
+                  backgroundColor: '#333 ', 
                   border: 'none',
                   borderRadius: '8px',
                   boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
